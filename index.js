f9e8d4431e65a2432c7ef920c4ee8272cf2dfca5
jsfather <keyvanmatinrad@outlook.com>
2024-11-23 22:51:45 +0330

    function now supports 0 to 999 numbers

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 export default function numberToPersianOrdinal(number) {
-    const ordinals = {
-        1: "اول",
-        2: "دوم",
-        3: "سوم",
-        4: "چهارم",
-        5: "پنجم",
-        6: "ششم",
-        7: "هفتم",
-        8: "هشتم",
-        9: "نهم",
-        10: "دهم",
-        11: "یازدهم",
-        12: "دوازدهم",
-        13: "سیزدهم",
-        14: "چهاردهم",
-        15: "پانزدهم",
-        16: "شانزدهم",
-        17: "هفدهم",
-        18: "هجدهم",
-        19: "نوزدهم",
-        20: "بیستم",
-    };
+    if (number < 0 || number > 999 || !Number.isInteger(number)) {
+        throw new Error("Number must be an integer between 0 and 999");
+    }
 
-    return ordinals[number];
+    if (number === 0) return "صفرم";
+
+    const units = ['', 'یک', 'دو', 'سه', 'چهار', 'پنج', 'شش', 'هفت', 'هشت', 'نه']
+    const teens = ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده'];
+    const tens = ['', 'ده', 'بیست', 'سی', 'چهل', 'پنجاه', 'شصت', 'هفتاد', 'هشتاد', 'نود'];
+    const hundreds = ['', 'یکصد', 'دویست', 'سیصد', 'چهارصد', 'پانصد', 'ششصد', 'هفتصد', 'هشتصد', 'نهصد'];
+
+    let word = '';
+    const hundred = Math.floor(number / 100);
+    const remainder = number % 100;
+
+    if (hundred > 0) {
+        word += hundreds[hundred] + (remainder !== 0 ? ' و ' : 'م');
+    }
+
+    if (remainder >= 10 && remainder < 20) {
+        word += teens[remainder - 10] + 'م';
+    } else {
+        const ten = Math.floor(remainder / 10);
+        const unit = remainder % 10;
+
+        if (ten > 0) {
+            word += tens[ten] + (unit !== 0 ? ' و ' : 'م');
+        }
+        if (unit > 0) {
+            if (unit === 3) {
+                word += 'سوم'
+            } else {
+                word += units[unit] + 'م';
+            }
+        }
+    }
+
+    return word.trim();
 }
